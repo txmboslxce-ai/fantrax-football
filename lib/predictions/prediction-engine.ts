8f0a77e64f790c107e273b3dc4429fcf731e39d4
Tim Dunn <txmboslxce@gmail.com>
2026-03-09 17:47:10 -0600

    Fix prediction engine feature row cast

--- lib/predictions/prediction-engine.ts
+++ lib/predictions/prediction-engine.ts
@@ -608,7 +608,7 @@ export async function loadPlayerPredictionFeatures(
     throw new Error(error.message);
   }
 
-  return (data ?? []) as PlayerPredictionFeatureRow[];
+  return (data ?? []) as unknown as PlayerPredictionFeatureRow[];
 }
 
 export function buildPredictions(
